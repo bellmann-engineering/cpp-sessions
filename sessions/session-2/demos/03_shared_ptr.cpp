41801e006307a99b3ec85393d8b292ca227a1ebc
archahmedelsha-lab <arch.ahmedelsha@gmail.com>
2026-02-25 11:27:43 +0100

    03_shared_ptr.cpp

--- sessions/session-2/demos/03_shared_ptr.cpp
+++ sessions/session-2/demos/03_shared_ptr.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <memory>
 
+class Resource {
+public:
+    Resource() { std::cout << "Resource erstellt\n"; }
+    ~Resource() { std::cout << "Resource zerstört\n"; }
+    void use() { std::cout << "Resource genutzt\n"; }
+};
+
+int main() {
+    std::shared_ptr<Resource> sp1 = std::make_shared<Resource>();
+    std::cout << "use_count: " << sp1.use_count() << std::endl;
+    {
+        std::shared_ptr<Resource> sp2 = sp1;
+        std::cout << "use_count: " << sp1.use_count() << std::endl;
+        sp2->use();
+    }
+    std::cout << "use_count: " << sp1.use_count() << std::endl;
+    sp1->use();
+    return 0;
+}
