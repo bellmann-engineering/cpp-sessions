f42b9ff63ab764498e09de81b33b34e905e251ff
archahmedelsha-lab <arch.ahmedelsha@gmail.com>
2026-03-02 17:11:21 +0100

    01_virtual_cost.cpp

--- sessions/session-6/demos/01_virtual_cost.cpp
+++ sessions/session-6/demos/01_virtual_cost.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+struct NoVirtual {
+    int a;
+    int b;
+};
+
+struct WithVirtual {
+    int a;
+    int b;
+    virtual void f() {}
+};
+
+int main() {
+    std::cout << "Größe ohne virtuelle Funktionen: " << sizeof(NoVirtual) << " Byte\n";
+    std::cout << "Größe mit virtueller Funktion:   " << sizeof(WithVirtual) << " Byte\n";
+    // Erwartung: mit vptr 4/8 Byte mehr
+    return 0;
+}
