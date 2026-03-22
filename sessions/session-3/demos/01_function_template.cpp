8b7bbc074d17b65bb0f3788755882f8b59777d32
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2026-03-22 20:54:46 +0100

    Update 01_function_template.cpp

--- sessions/session-3/demos/01_function_template.cpp
+++ sessions/session-3/demos/01_function_template.cpp
@@ -9,5 +9,6 @@ int main() {
     std::cout << max(3, 7) << std::endl;
     std::cout << max(3.14, 2.72) << std::endl;
     std::cout << max('a', 'z') << std::endl;
+    std::cout << max<double>(3, 3.14) << std::endl; // erzwingt T = double -> 3 wird implizit zu 3.0 konvertiert, Vergleich erfolgt als double
     return 0;
 }
